7aa5bfb4f4e1ea53c21e9d6a340c16291b9893c3
Carina Mendes <carina.1574@gmail.com>
2026-05-01 12:26:04 -0300

    test: teste do middleware

--- estoques-pi-univesp/middleware.ts
+++ estoques-pi-univesp/middleware.ts
@@ -1,50 +1,56 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { getToken } from "next-auth/jwt";
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+// export async function middleware(req: NextRequest) {
+//   const token = await getToken({
+//     req,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   });
 
-  const { pathname } = req.nextUrl;
+//   const { pathname } = req.nextUrl;
 
-  // rotas públicas
-  const publicRoutes = ["/", "/novo-usuario"];
+//   // rotas públicas
+//   const publicRoutes = ["/", "/novo-usuario"];
 
-  // arquivos estáticos e API do NextAuth
-  const isPublicFile =
-    pathname.startsWith("/_next") ||
-    pathname.startsWith("/api/auth") ||
-    pathname.startsWith("/api/usuarios") ||
-    pathname.includes(".");
+//   // arquivos estáticos e API do NextAuth
+//   const isPublicFile =
+//     pathname.startsWith("/_next") ||
+//     pathname.startsWith("/api/auth") ||
+//     pathname.startsWith("/api/usuarios") ||
+//     pathname.includes(".");
 
-  if (isPublicFile) {
-    return NextResponse.next();
-  }
+//   if (isPublicFile) {
+//     return NextResponse.next();
+//   }
 
-  // se não está logado e tenta acessar rota privada
-  if (!token && !publicRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+//   // se não está logado e tenta acessar rota privada
+//   if (!token && !publicRoutes.includes(pathname)) {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
 
-  // se está logado e tenta acessar login
-  if (token && pathname === "/") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
+//   // se está logado e tenta acessar login
+//   if (token && pathname === "/") {
+//     return NextResponse.redirect(new URL("/dashboard", req.url));
+//   }
 
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: [
+//     /*
+//      * Protege tudo exceto:
+//      * - api/auth (NextAuth)
+//      * - _next (assets internos)
+//      * - arquivos estáticos
+//      */
+//     "/((?!api/auth|_next|favicon.ico).*)",
+//   ],
+// };
+
+import { NextResponse } from 'next/server';
+
+export function middleware() {
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    /*
-     * Protege tudo exceto:
-     * - api/auth (NextAuth)
-     * - _next (assets internos)
-     * - arquivos estáticos
-     */
-    "/((?!api/auth|_next|favicon.ico).*)",
-  ],
-};
